8352968c5b1948295fc0675c50f63e0aaf4d167c
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-11-08 13:34:35 -0600

    testing navigations and failing

---
+++ firstRNapp/navigations/Navigator.js
@@ -0,0 +1,22 @@
+// import { createAppContainer } from 'react-navigation';
+// import { createStackNavigator } from 'react-navigation-stack';
+// import Factorial from '../components/Factorial';
+//
+// const Navigator = createStackNavigator({
+//     Factorial: Factorial,
+// });
+//
+// export default createAppContainer(Navigator);
+
+
+
+
+//https://facebook.github.io/react-native/docs/navigation
+
+//npm install ....
+//react-navigation
+//react-native-gesture-handler
+//react-navigation-stack
+
+
+//CRASHES with error message: cannot find module "12"

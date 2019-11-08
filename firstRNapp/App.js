9bf0693fefb5f04f55be502f2e8f97e5cacb695a
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-11-08 11:26:31 -0600

    factorial

---
+++ firstRNapp/App.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {num: ''}
+  }
+
+  render() {
+    factorial = (num) => {
+      let result = num
+      if (num === 0 || num === 1) {
+        return 1
+      };
+      while (num > 1) {
+        num--;
+        result = num * result;
+      }
+      return <Text> {result} </Text>
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Find the Factorial!</Text>
+        <TextInput style={styles.input}
+        placeholder="Enter Integer"
+        onChangeText={(num) => this.setState({num})}
+        value={this.state.num}
+        >
+        </TextInput>
+        <Text style={styles.answer}>{factorial(this.state.num)}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 42,
+  },
+  input: {
+    height: 40,
+    width: 200,
+    textAlign: 'center',
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: 'blue',
+    fontSize: 30,
+  },
+  answer: {
+    fontSize: 30,
+  }
+});
